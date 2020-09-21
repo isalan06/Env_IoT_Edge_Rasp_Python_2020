91478597a6083b8598dc0ec9f95e75f1a2154ea8
isalan06 <isalan06@gmail.com>
2020-09-21 10:28:29 +0800

    test update

--- examples/Demo_2009.py
+++ examples/Demo_2009.py
@@ -6,8 +6,8 @@ import adafruit_dht
 
 # DHT22 Attribute
 dhtDevice = adafruit_dht.DHT22(board.D17)
-temp_c = 0.0
-humidity = 0
+temp_c=0.0
+humidity=0
 
 while True:
     time.sleep(10.0)
@@ -16,7 +16,7 @@ while True:
     try:
         temp_c = dhtDevice.temperature
         humidity = dhtDevice.humidity
-        print("Temp: {:.1f}C Humidity: {}%", format(temp_c, humidity))
+        print("Temp: {:.1f}C Humidity: {}%".format(temp_c, humidity))
     except RuntimeError as error:
         print(error.args[0])
         continue
