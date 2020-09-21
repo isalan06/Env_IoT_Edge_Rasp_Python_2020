5b35c16d991d412b970109a4ff45365885d82ffc
Strawberry Huang <alan.huang6@msa.hinet.net>
2020-09-21 14:41:55 +0800

    Update

--- examples/Demo_2009.py
+++ examples/Demo_2009.py
@@ -3,6 +3,7 @@ import math
 import time
 import board
 import adafruit_dht
+import json
 
 # DHT22 Attribute
 dhtDevice = adafruit_dht.DHT22(board.D17)
@@ -20,3 +21,38 @@ while True:
     except RuntimeError as error:
         print(error.args[0])
         continue
+
+    #JSON
+    SetKey="Machine"
+    SetValue="IoT Edge"
+    InformationData = {}
+    InformationData[SetKey]=SetValue
+    InformationData["Machine ID"]="Test ID"
+    InformationData["Comm Type"]="4G"
+    SetKey="Data"
+    InformationData[SetKey]={}
+    SetKey2="Temp"
+    SetKey3="Data"
+    InformationData[SetKey][SetKey2]={}
+    InformationData[SetKey][SetKey2]["Count"]=1
+    InformationData[SetKey][SetKey2][SetKey3]={}
+    InformationData[SetKey][SetKey2][SetKey3]["ID"]=1
+    InformationData[SetKey][SetKey2][SetKey3]["Type"]="Gateway"
+    InformationData[SetKey][SetKey2][SetKey3]["Unit"]="C"
+    InformationData[SetKey][SetKey2][SetKey3]["Value"]=temp_c
+    SetKey2="Humidity"
+    InformationData[SetKey][SetKey2]={}
+    InformationData[SetKey][SetKey2]["Count"]=1
+    InformationData[SetKey][SetKey2][SetKey3]={}
+    InformationData[SetKey][SetKey2][SetKey3]["ID"]=1
+    InformationData[SetKey][SetKey2][SetKey3]["Type"]="Gateway"
+    InformationData[SetKey][SetKey2][SetKey3]["Unit"]="%RH"
+    InformationData[SetKey][SetKey2][SetKey3]["Value"]=humidity
+
+    TransferJSONData=json.dumps(InformationData)
+    print(TransferJSONData)
+
+
+
+
+
