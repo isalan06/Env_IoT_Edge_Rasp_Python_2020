d3e2425b156e9cfcc499ed56a886d684f274c6d8
Strawberry Huang <alan.huang6@msa.hinet.net>
2020-09-21 09:43:44 +0800

    Add New Program for Test

---
+++ examples/Demo_2009.py
@@ -0,0 +1,22 @@
+import smbus
+import math
+import time
+import board
+import adafruit_dht
+
+# DHT22 Attribute
+dhtDevice = adafruit_dht.DHT22(board.D17)
+temp_c = 0.0
+humidity = 0
+
+while True:
+    time.sleep(10.0)
+
+    #DHT22
+    try:
+        temp_c = dhtDevice.temperature
+        humidity = dhtDevice.humidity
+        print("Temp: {:.1f}C Humidity: {}%", format(temp_c, humidity))
+    except RuntimeError as error:
+        print(error.args[0])
+        continue
