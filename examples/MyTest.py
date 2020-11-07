c7e083b760dc3dd9363df58f80fb575f082bf9e4
isalan06 <isalan06@gmail.com>
2020-11-07 23:04:37 +0800

    update

--- examples/MyTest.py
+++ examples/MyTest.py
@@ -22,13 +22,14 @@ with picamera.PiCamera() as camera:
     camera.resolution = (1024,768)
     time.sleep(1.0)
     camera.capture(fileString)
-
+print("Ping Response:")
+print(response)
 if response == 0:
     setsn=1
     setfilename=filename
     setdatetime=nowtime.strftime('%Y%m%d%H%M%S')
-    url = "http://192.168.8.100:5099/Update/JpgPicture?sn=" + setsn + "&filename=" + setfilename + "&datetime=" + setdatetime
-    file=open('test','rb')
+    url = "http://192.168.8.100:5099/Update/JpgPicture?sn=" + str(setsn) + "&filename=" + setfilename + "&datetime=" + setdatetime
+    file=open(fileString ,'rb')
     payload=file.read()
     file.close()
     headers = {'Content-Type': 'image/jpeg'}
