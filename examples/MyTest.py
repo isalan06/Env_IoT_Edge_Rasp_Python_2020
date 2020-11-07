08a723bbf6ef56cdafede3c9be0a9e1a75a544f9
isalan06 <45954407+isalan06@users.noreply.github.com>
2020-11-07 17:11:54 +0800

    update

--- examples/MyTest.py
+++ examples/MyTest.py
@@ -1,1 +1,22 @@
-print("test")
+#!/usr/bin/python3
+
+import os
+import picamera
+import time
+from datetime import datetime
+
+
+nowtime = datetime.now()
+datestring = nowtime.strftime('%Y%m%d')
+fileString ="Pictures/" + datestring + "/"
+if not os.path.isdir("Pictures/"):
+    os.mkdir("Pictures/")
+if not os.path.isdir(fileString):
+    os.mkdir(fileString)
+fileString += datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+fileString += ".jpg"
+
+with picamera.PiCamera() as camera:
+    camera.resolution = (1024,768)
+    time.sleep(1.0)
+    camera.capture(fileString)
