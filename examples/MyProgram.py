e89d4329d31c8feb39b90f773485511c15b1287c
isalan06 <45954407+isalan06@users.noreply.github.com>
2020-11-08 23:42:48 +0800

    update

--- examples/MyProgram.py
+++ examples/MyProgram.py
@@ -388,7 +388,36 @@ def GetCommandFromCloud():
             CapturePictureRV=data['CapturePictureRV']
             CaptureVideoSecond=data['CaptureVideoSecond']
             print("Set Value Completely")
+        if _command == "":
+            bconnected = os.system("ping -c 1 192.168.8.100")
 
+            nowtime = datetime.now()
+            datestring = nowtime.strftime('%Y%m%d')
+            fileString ="CapPictures/" + datestring + "/"
+
+            if not os.path.isdir("CapPictures/"):
+                os.mkdir("CapPictures/")
+            if not os.path.isdir(fileString):
+                os.mkdir(fileString)
+            filename = nowtime.strftime('%Y%m%d%H%M%S') + ".jpg"
+            fileString += filename
+
+            with picamera.PiCamera() as camera:
+                camera.resolution = (CapturePictureRH,CapturePictureRV)
+                time.sleep(1.0)
+                camera.capture(fileString)
+
+            if bconnected == 0:
+                setsn=1
+                setfilename=filename
+                setdatetime=nowtime.strftime('%Y%m%d%H%M%S')
+                url = "http://192.168.8.100:5099/Update/JpgCapPicture?sn=" + str(setsn) + "&filename=" + setfilename + "&datetime=" + setdatetime
+                file=open(fileString ,'rb')
+                payload=file.read()
+                file.close()
+                headers = {'Content-Type': 'image/jpeg'}
+                responses = requests.request("POST", url, headers=headers, data = payload)
+                print(responses.text.encode('utf8'))
 
         time.sleep(1.0)
 
