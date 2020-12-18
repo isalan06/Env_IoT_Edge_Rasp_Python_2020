e8542121341377c183fbca70d62199e68f331b43
isalan06 <45954407+isalan06@users.noreply.github.com>
2020-12-18 21:47:55 +0800

    add

---
+++ examples/MyBLETesting_c.py
@@ -0,0 +1,88 @@
+import sys
+import struct
+from bluepy.btle import UUID, Peripheral
+from bluepy import btle
+
+class MyDelegate(btle.DefaultDelegate):
+    def __init__(self):
+        btle.DefaultDelegate.__init__(self)
+        print("Delegate initial Success")
+
+    def handleNotification(self, cHandle, data):
+            #print(data)
+            if len(data) >= 3:
+                data1 = data[0]
+                data2 = data[1]
+                data3 = data[2]
+                data4 = float(data2 * 256 + data1) / 100.0
+                print("Get Temp:" + str(data4) + "C;   Humidity:" + str(data3) + "%RH")
+
+class MyTest():
+    def __init__(self, index, mac_address):
+        self.index=index
+        self.mac_address=mac_address
+
+    def Run(self):
+        print("Start To Connect BLE")
+        p = Peripheral(self.mac_address)
+        p.setDelegate(MyDelegate())
+        try:
+            chList=p.getCharacteristics()
+
+
+            seList=p.getServices()
+            for se in seList:
+                try:
+                    print(se.uuid)
+                except:
+                    print("SE Error")
+
+            print("---------------------------")
+            print("Get Service1")
+
+
+            try:
+                se1=p.getServiceByUUID('0000180f-0000-1000-8000-00805f9b34fb')
+                ch1=se1.getCharacteristics('00002a19-0000-1000-8000-00805f9b34fb')
+
+                for _ch1 in ch1:
+                    print(_ch1.uuid)
+
+                print("----------------------------------")
+                print(ch1[0].uuid)
+                print(ch1[0].read())
+            except:
+                print("SE1 Error")
+
+            print("-------------------------------------------")
+            print("Get Data")
+            try:
+                se10=p.getServiceByUUID('ebe0ccb0-7a0a-4b0c-8a1a-6ff2997da3a6')
+                print("------------------------")
+                print(se10)
+                ch10=se10.getCharacteristics('ebe0ccc1-7a0a-4b0c-8a1a-6ff2997da3a6')
+                print("-------------------------")
+                print(ch10)
+                print("---------------------------------")
+
+            except:
+                print("Get Data Error")
+
+
+            for i in range(1, 100):
+                if p.waitForNotifications(1.0):
+                    print("----------------------")
+
+        except:
+            print("Connect Fail")
+        finally:
+            print("Finish")
+            p.disconnect()
+
+def main(self):
+    myTest = MyTest(1, 'a4:c1:38:0b:99:ed')
+
+    myTest.Run()
+
+if __name__ == "__main__":
+    main()
