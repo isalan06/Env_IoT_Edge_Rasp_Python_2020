6967afb6429f2a4ef1c69144cbc565d9332ef6c3
isalan06 <45954407+isalan06@users.noreply.github.com>
2020-12-24 23:49:48 +0800

    update

--- examples/MyFtpTesting.py
+++ examples/MyFtpTesting.py
@@ -4,7 +4,7 @@ IP = '122.116.123.236'
 user = 'uploaduser'
 password = 'antiupload3t6Q'
 filename = 'sn_2020-11-11 16-35-28-000.jpg'
-path = '~/download/sn_2020-11-11_16-35-28-000.jpg'
+path = '/home/pi/download/sn_2020-11-11_16-35-28-000.jpg'
 ftp=FTP() 
 ftp.set_debuglevel(2) 
 ftp.connect(IP) 
