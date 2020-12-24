24d8fd2e3017f4148f9f04805b7ae3bc56a5d58c
isalan06 <45954407+isalan06@users.noreply.github.com>
2020-12-25 00:01:06 +0800

    update

--- examples/MyFtpTesting.py
+++ examples/MyFtpTesting.py
@@ -3,7 +3,7 @@ from ftplib import FTP
 IP = '122.116.123.236'
 user = 'uploaduser'
 password = 'antiupload3t6Q'
-filename = 'sn_2020-11-11 16-35-28-000.jpg'
+filename = 'sn_2020-11-11_16-35-28-000.jpg'
 path = '/home/pi/download/sn_2020-11-11_16-35-28-000.jpg'
 ftp=FTP() 
 ftp.set_debuglevel(2) 
