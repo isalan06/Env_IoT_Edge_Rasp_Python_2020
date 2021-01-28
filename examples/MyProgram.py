41d3213d3e486a651e639cd2f5460d3e0c946ac7
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-01-28 13:51:54 +0800

    update 2

--- examples/MyProgram.py
+++ examples/MyProgram.py
@@ -66,7 +66,8 @@ else:
 
 def get_mac_address():
     mac=uuid.UUID(int = uuid.getnode()).hex[-12:]
-    return ":".join([mac[e:e+2] for e in range(0,11,2)])
+    #return ":".join([mac[e:e+2] for e in range(0,11,2)])
+    return mac
 
 # BLE Program
 #region
