93861ca4bfea07ff0b93be9975d037b5ecb891cf
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-02-12 23:59:31 +0800

    update

--- examples/MyPyDriveTest.py
+++ examples/MyPyDriveTest.py
@@ -5,7 +5,6 @@ gauth = GoogleAuth()
 gauth.CommandLineAuth() 
 drive = GoogleDrive(gauth)
 
-file1 = drive.CreateFile({'title': 'Hello.txt','parents':[{'kind': 'drive#fileLink',
-                                     'id': '1eHnor5HRg9eTAlpvcwGigBw74Inkb4eL'}]}) 
+file1 = drive.CreateFile({'title': 'Hello.txt'}) 
 file1.SetContentString('Hello World!')
 file1.Upload() 
