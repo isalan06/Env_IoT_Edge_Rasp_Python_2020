bbb4701886c4be8fc0d0a632a2d44860a787c6cc
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-02-16 10:01:34 +0800

    update

--- examples/MyProgram.py
+++ examples/MyProgram.py
@@ -775,14 +775,15 @@ def GetSensorsData():
                     bRecordVibration = False
                     calCount_RecordVibration = 0
                     TransferJSONData=json.dumps(RecordVibrationData)
-                    #try:
-                    auth=('token', 'example')
-                    ssl._create_default_https_context = ssl._create_unverified_context
-                    headers = {'Content-Type': 'application/json'}
-                    r = requests.post('https://script.google.com/macros/s/AKfycbyaqQfJagU3KR5ccgIfWkD99dLLtn-NQJbwNJ9siPdVU7VJsoA/exec',headers=headers, data=TransferJSONData, auth=auth)
-                    print(ANSI_Green + "--Update Record Vibration Success" + ANSI_OFF)
-                    #except BaseException as error:
-                        #print(ANSI_RED + "--Update Record Vibration Failure" + ANSI_OFF)
+                    try:
+                        auth=('token', 'example')
+                        ssl._create_default_https_context = ssl._create_unverified_context
+                        headers = {'Content-Type': 'application/json'}
+                        r = requests.post('https://script.google.com/macros/s/AKfycbyaqQfJagU3KR5ccgIfWkD99dLLtn-NQJbwNJ9siPdVU7VJsoA/exec',headers=headers, data=TransferJSONData, auth=auth)
+                        print(ANSI_Green + "--Update Record Vibration Success" + ANSI_OFF)
+                    except BaseException as error:
+                        print(ANSI_RED + "--Update Record Vibration Failure" + ANSI_OFF)
+                        print(error)
 
             sAccelGaugeStatus = "Running"
         except:
