a5c01e1bca229eb8152fe2a77696ef62a9de9f41
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-04-14 09:50:17 +0800

    update

--- examples/MyProgram.py
+++ examples/MyProgram.py
@@ -1219,7 +1219,7 @@ def getDrive(drive=None, gauth=None):
             gauth = GoogleAuth()#(settings_file=settings_yaml)
         # Try to load saved client credentials
         print("Start to Load Credential")
-        gauth.LoadCredentialsFile(gauth)
+        gauth.LoadCredentialsFile("credentials.json")
         print("Load Credential")
         if gauth.access_token_expired:
             # Refresh them if expired
