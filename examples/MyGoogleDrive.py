b221f9044b66410ab2a5c5c678760eb82ab05a09
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-04-14 18:45:15 +0800

    update

--- examples/MyGoogleDrive.py
+++ examples/MyGoogleDrive.py
@@ -6,6 +6,7 @@ import os
 import MyParameter
 
 from googleapiclient.discovery import build
+from googleapiclient.http import MediaFileUpload
 from google_auth_oauthlib.flow import InstalledAppFlow
 from google.auth.transport.requests import Request
 from google.oauth2.credentials import Credentials
