46950bff18534072c669dfdf74009d6c5dcc11e6
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-04-18 16:00:01 +0800

    update

--- examples/MySerialPortTest.py
+++ examples/MySerialPortTest.py
@@ -26,15 +26,7 @@ while res != b'':
 
 print(buffer)
 
-data_byte1 = []
-data_byte1.append(buffer[4])
-data_byte1.append(buffer[5])
-data_bytes1 = bytes(data_byte1)
-data_byte2 = []
-data_byte2.append(buffer[6])
-data_byte2.append(buffer[7])
-data_bytes2 = bytes(data_byte2)
-data1 = int.from_bytes(data_bytes1, 'big')
-data2 = int.from_bytes(data_bytes2, 'big')
+data1 = int.from_bytes(buffer[4:5], 'big')
+data2 = int.from_bytes(buffer[6:7], 'big')
 print(data1)
 print(data2)
