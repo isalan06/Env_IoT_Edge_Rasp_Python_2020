95c3e73bdc15f2371e16221be7c0654d16d7c49c
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-04-18 16:04:46 +0800

    test

--- examples/MySerialPortTest.py
+++ examples/MySerialPortTest.py
@@ -16,17 +16,20 @@ outputCommand = b'\xA5\x09\xAE'
 ser.write(outputCommand)
 
 buffer = []
+buffer2 = b''
 
 res = ser.read()
 
 while res != b'':
     print(res)
     buffer.append(res)
+    buffer2 = buffer2 + res
     res = ser.read()
 
 print(buffer)
+print(buffer2)
 
-data1 = int.from_bytes(buffer[4:5], 'big')
-data2 = int.from_bytes(buffer[6:7], 'big')
-print(data1)
-print(data2)
+#data1 = int.from_bytes(buffer[4:5], 'big')
+#data2 = int.from_bytes(buffer[6:7], 'big')
+#print(data1)
+#print(data2)
