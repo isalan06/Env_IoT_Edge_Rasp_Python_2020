d5d5aab5f93ce7077d9d2af4c9a7d781889f84b2
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-04-18 16:03:50 +0800

    update

---
+++ MySerialPortTest.py
@@ -0,0 +1,35 @@
+import serial
+
+ser=serial.Serial(
+    port='/dev/ttyS0',
+    baudrate=9600,
+    parity=serial.PARITY_NONE,
+    stopbits=serial.STOPBITS_ONE,
+    bytesize=serial.EIGHTBITS,
+    timeout=1    
+)
+
+ser.flushInput()
+
+outputCommand = b'\xA5\x09\xAE'
+
+ser.write(outputCommand)
+
+buffer = []
+buffer2 = b''
+
+res = ser.read()
+
+while res != b'':
+    print(res)
+    buffer.append(res)
+    buffer2 = buffer2 + res
+    res = ser.read()
+
+print(buffer)
+print(buffer2)
+
+#data1 = int.from_bytes(buffer[4:5], 'big')
+#data2 = int.from_bytes(buffer[6:7], 'big')
+#print(data1)
+#print(data2)
