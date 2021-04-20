4ee3d9836bd2e1c8e57bbb06e208480d68148fea
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-04-20 19:48:52 +0800

    update

--- examples/MyCameraTest.py
+++ examples/MyCameraTest.py
@@ -12,6 +12,12 @@ time.sleep(0.1)
 # grab an image from the camera
 camera.capture(rawCapture, format="bgr")
 image = rawCapture.array
+
+
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+print("Light:", gray_image.mean())
+
 # display the image on screen and wait for a keypress
 cv2.imshow("Image", image)
 cv2.waitKey(0)
+
