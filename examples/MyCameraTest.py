c7dafa8c2e1afb6aa9096725a08a3359c20daa64
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-05-31 08:56:15 +0800

    update

--- examples/MyCameraTest.py
+++ examples/MyCameraTest.py
@@ -3,6 +3,19 @@ from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
 import cv2
+
+import base64
+from PIL import Image
+from io import BytesIO
+
+def frame2base64(frame):
+    img = Image.fromarray(frame) #將每一幀轉為Image
+    output_buffer = BytesIO() #建立一個BytesIO
+    img.save(output_buffer, format='JPEG') #寫入output_buffer
+    byte_data = output_buffer.getvalue() #在記憶體中讀取
+    sSmallImageData = base64.b64encode(byte_data) #轉為BASE64
+    print(sSmallImageData)
+
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (1600,900)
@@ -16,6 +29,7 @@ image = rawCapture.array
 
 gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 print("Light:", gray_image.mean())
+frame2base64(rawCapture)
 
 # display the image on screen and wait for a keypress
 cv2.imshow("Image", image)
