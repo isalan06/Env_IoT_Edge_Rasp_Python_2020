77810cb83344833d00a37607abdb880e82bb1c4b
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-06-14 09:51:37 +0800

    update

--- examples/MyCamera.py
+++ examples/MyCamera.py
@@ -156,8 +156,8 @@ def DoWork():
                 with picamera.PiCamera() as camera:
                     #camera.resolution = (480, 320)
                     #rawCapture = PiRGBArray(camera, size=(480, 320))
-                    camera.resolution = (960, 540)
-                    rawCapture = PiRGBArray(camera, size=(960, 540))
+                    camera.resolution = (480, 320)
+                    rawCapture = PiRGBArray(camera, size=(480, 320))
                     # allow the camera to warmup
                     time.sleep(0.1)
                     # grab an image from the camera
