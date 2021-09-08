a6b21d56e561c23cd56faf95af844767658f06da
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-09-08 15:43:16 +0800

    update

--- gui/gui_test.py
+++ gui/gui_test.py
@@ -1,4 +1,6 @@
-from guizero import App
+from guizero import App, Text
+
+welcome_message = Text(app, text="Welcome to my app", size=40, font="Times New Roman", color="lightblue")
 
 app = App(title="Hello world")
 app.display()
