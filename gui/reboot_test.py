192cd9ccef3ba4edb4168242fab5b52306b39cd3
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-09-10 14:55:46 +0800

    update

--- gui/reboot_test.py
+++ gui/reboot_test.py
@@ -1,8 +1,14 @@
 from datetime import datetime
 
+test_index = 0
+
 print(datetime.now().strftime("%H"))
 if datetime.now().strftime("%H") == "14":
+    test_index = 1
     print("14")
-if datetime.now().start_time("%H") == "15":
+if datetime.now().strftime("%H") == "15":
     print("15")
-            
+
+
+if (datetime.now().strftime("%H") == "14") and (test_index == 1):
+    print("AAA")
