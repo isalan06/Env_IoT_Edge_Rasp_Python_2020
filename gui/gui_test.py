951dcbac5706e5a69fe36b25741e1c3ac83f097a
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-09-11 15:34:03 +0800

    update

--- gui/gui_test.py
+++ gui/gui_test.py
@@ -1,12 +1,16 @@
-from guizero import App, Text, TextBox, PushButton
+from guizero import App, Text, TextBox, PushButton, Slider
 
 def say_my_name():
     welcome_message.value = my_name.value
+
+def change_text_size(slider_value):
+    welcome_message.size = slider_value
 
 app = App(title="Hello world")
 
 welcome_message = Text(app, text="Welcome to my app", size=40, font="Times New Roman", color="lightblue")
 my_name = TextBox(app, text="Type here", width=100)
 update_text = PushButton(app, command=say_my_name, text="Display my name")
+text_size = Slider(app, command=change_text_size, start = 10, end=80)
 
 app.display()
