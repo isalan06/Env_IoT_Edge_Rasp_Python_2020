ff78962fa661309e8f01e2ab1fc5ed2b79e7028b
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-09-11 21:17:25 +0800

    update

--- gui/gui_test.py
+++ gui/gui_test.py
@@ -6,11 +6,15 @@ def say_my_name():
 def change_text_size(slider_value):
     welcome_message.size = slider_value
 
+def close_app():
+    app.destroy()
+
 app = App(title="Hello world")
 
 welcome_message = Text(app, text="Welcome to my app", size=40, font="Times New Roman", color="lightblue")
 my_name = TextBox(app, text="Type here", width=100)
 update_text = PushButton(app, command=say_my_name, text="Display my name")
+close_text = PushButton(app, command=close_app, text="Close")
 text_size = Slider(app, command=change_text_size, start = 10, end=80)
 my_cat = Picture(app, image="test.jpg", width=200, height = 100)
 
