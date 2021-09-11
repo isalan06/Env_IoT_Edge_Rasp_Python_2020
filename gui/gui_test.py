59fffd07c446d4f856f6faf0ad0c94588a360352
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-09-11 15:44:23 +0800

    test

--- gui/gui_test.py
+++ gui/gui_test.py
@@ -12,6 +12,6 @@ welcome_message = Text(app, text="Welcome to my app", size=40, font="Times New R
 my_name = TextBox(app, text="Type here", width=100)
 update_text = PushButton(app, command=say_my_name, text="Display my name")
 text_size = Slider(app, command=change_text_size, start = 10, end=80)
-my_cat = Picture(app, image="test.jpg")
+my_cat = Picture(app, image="test.jpg", width=200, height = 100)
 
 app.display()
