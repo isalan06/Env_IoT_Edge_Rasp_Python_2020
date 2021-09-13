1eec5a51bec7ebd76d2d6935ff0b60b71808f3c2
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-09-13 23:48:48 +0800

    update

--- gui/api_test.py
+++ gui/api_test.py
@@ -1,6 +1,7 @@
 #!flask/bin/python
 from flask import Flask
 from flask import request
+import os
 
 app = Flask(__name__)
 
@@ -16,8 +17,28 @@ def qrcode():
     if data and "qrcode" in data:
         qrcode = data["qrcode"]
     temperature = data["temperature"]
+    createtime = data['createDate']
+    sensortype = data['type']
     print('ID:' + qrcode)
     print('Temperature:' + str(temperature))
+
+    triggerfilename = '/home/pi/Data/trigger.txt'
+    if path.exists(triggerfilename):
+        os.remove(triggerfilename)
+    infofilename = '/home/pi/Data/info.txt'
+    if path.exists(infofilename):
+        os.remove(infofilename)
+    imagefilename = '/home/pi/Data/person.jpg'
+    if path.exists(imagefilename):
+        os.remove(imagefilename)
+
+    f = open(infofilename, 'w')
+    f.write(qrcode)
+    f.write(temperature)
+    f.write(createtime)
+    f.write(sensortype)
+    f.close()
+
 
     return "OK"
 
