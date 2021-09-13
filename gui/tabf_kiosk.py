9a83d545d430d1c18e888f037cd4c0889727c416
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-09-13 21:23:37 +0800

    update

--- gui/tabf_kiosk.py
+++ gui/tabf_kiosk.py
@@ -202,9 +202,9 @@ print('TABF KIOSK Program start...')
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "Hello, World!"
+@app.route("/", methods=["POST", "GET"])
+def home():
+	return "Hello"
 
 
 
@@ -212,12 +212,14 @@ def index():
 #ApiServerThread.start()
 
 #def Display_Dowork():
-if __name__ == '__Main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+	app.run(debug=True)
 
-    kwargs = {'host': '127.0.0.1', 'port': 5000, 'threaded': True, 'use_reloader': False, 'debug': True}
+#   Preparing parameters for flask to be given in the thread
+#   so that it doesn't collide with main thread
+	kwargs = {'host': '127.0.0.1', 'port': 5000, 'threaded': True, 'use_reloader': False, 'debug': False}
 
-    #   running flask thread
+#   running flask thread
 	flaskThread = Thread(target=app.run, daemon=True, kwargs=kwargs).start()
 
     print('Show Display')
