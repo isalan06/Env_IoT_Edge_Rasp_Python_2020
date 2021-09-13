1dc2b9e153477f44b42fcfb5d77765a8bdf02d4e
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-09-13 10:42:26 +0800

    update

--- gui/tabf_kiosk.py
+++ gui/tabf_kiosk.py
@@ -20,6 +20,14 @@ def APIServer_DoWork():
     except:
         bListenFail = True
 
+def ExecuteProcedure():
+    print('ExecuteProcedure')
+
+def OpenTestForm():
+    global bOpenTestForm
+    bOpenTestForm = True
+    app.destroy()
+
 print('TABF KIOSK Program start...')
 
 apiserver = Flask(__name__)
@@ -27,11 +35,20 @@ apiserver = Flask(__name__)
 ApiServerThread = threading.Thread(target=APIServer_DoWork)
 ApiServerThread.start()
 
-app = App(title='TABF 報到機 Ver2.0', width=300, height = 200, layout="grid")
+app = App(title='TABF 報到機 Ver2.0', width=600, height =350, layout="grid")
 
+_app_showLabel = Text(app, text="TABF 報到機操作介面", size=24, font="Times New Roman", color="black", grid = [0,0])
+_executeProcedure = PushButton(app, grid=[0,1], command=ExecuteProcedure, text='執行報到資料下載')
+_opentestform = PushButton(app, grid=[0,2], command=OpenTestForm, test='開啟報到測試模式')
 
 
 app.display()
+
+if bOpenTestForm == True:
+    app2 = (title='TABF 報到機 測試畫面', width=600, height =350, layout="grid")
+    app2.set_full_screen('Esc')
+
+    app2.display()
 
 if bListenFail == False:
     shutdown_server()
