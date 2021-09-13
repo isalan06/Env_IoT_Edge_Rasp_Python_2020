ad6df560e0e95631b186d7b0c41c281cae48c410
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-09-13 20:28:42 +0800

    update

---
+++ gui/api_test2.py
@@ -0,0 +1,19 @@
+from flask import *
+from threading import Thread
+
+app = Flask(__name__)
+
+@app.route("/", methods=["POST", "GET"])
+def home():
+	return "Hello"
+
+
+if __name__ == "__main__":
+	app.run(debug=True)
+
+#   Preparing parameters for flask to be given in the thread
+#   so that it doesn't collide with main thread
+	kwargs = {'host': '127.0.0.1', 'port': 5000, 'threaded': True, 'use_reloader': False, 'debug': False}
+
+#   running flask thread
+	flaskThread = Thread(target=app.run, daemon=True, kwargs=kwargs).start()
