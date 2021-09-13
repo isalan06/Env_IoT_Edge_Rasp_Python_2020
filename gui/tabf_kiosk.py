e717dfcbceea9c07f706af7b0241457be883b725
isalan06 <45954407+isalan06@users.noreply.github.com>
2021-09-13 10:47:28 +0800

    update

--- gui/tabf_kiosk.py
+++ gui/tabf_kiosk.py
@@ -28,6 +28,9 @@ def OpenTestForm():
     bOpenTestForm = True
     app.destroy()
 
+def CloseTestForm():
+    app2.destroy()
+
 print('TABF KIOSK Program start...')
 
 apiserver = Flask(__name__)
@@ -38,8 +41,8 @@ ApiServerThread.start()
 app = App(title='TABF 報到機 Ver2.0', width=600, height =350, layout="grid")
 
 _app_showLabel = Text(app, text="TABF 報到機操作介面", size=24, font="Times New Roman", color="black", grid = [0,0])
-_executeProcedure = PushButton(app, grid=[0,1], command=ExecuteProcedure, text='執行報到資料下載')
-_opentestform = PushButton(app, grid=[0,2], command=OpenTestForm, text='開啟報到測試模式')
+_executeProcedure = PushButton(app, grid=[0,1], command=ExecuteProcedure, text='執行報到資料下載', align="left")
+_opentestform = PushButton(app, grid=[0,2], command=OpenTestForm, text='開啟報到測試模式', align="left")
 
 
 app.display()
@@ -47,6 +50,9 @@ app.display()
 if bOpenTestForm == True:
     app2 = App(title='TABF 報到機 測試畫面', width=600, height =350, layout="grid")
     app2.set_full_screen('Esc')
+
+    _app2_showLabel = Text(app2, text="TABF 報到測試模式", size=24, font="Times New Roman", color="black", grid = [0,0])
+    _closetestform = PushButton(app2, grid=[1,0], command=CloseTestForm, text='關閉', align="right")
 
     app2.display()
 
