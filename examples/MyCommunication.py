83048bc9d29540b11b4b229fc9fd507f40b62c7d
isalan06 <isalan06@gmail.com>
2022-10-09 23:20:41 +0800

    update

--- examples/MyCommunication.py
+++ examples/MyCommunication.py
@@ -135,7 +135,7 @@ def DoWork(macaddress):
 
     checkFunctionIntervalTime = time.time() - tCheckTimer_Start
 
-    if MyParameter.IsDataPlatformConnected and (checkFunctionIntervalTime >= 2):
+    if MyParameter.IsDataPlatformConnected and (checkFunctionIntervalTime >= 5):
         tCheckTimer_Start = time.time()
         print(ANSI_WHITE + '[Info] Start to communication data platform!' + ANSI_OFF)
         UpdateMachineStatus(macaddress)
