4eaaf4eaa6dc4e01b353098d875fa5653416f31a
isalan06 <isalan06@gmail.com>
2023-11-03 14:34:20 +0800

    update

--- spi_test/MySPIMaster.py
+++ spi_test/MySPIMaster.py
@@ -1,2 +1,20 @@
 #!/usr/bin/python3
 #MySPIMaster.py
+
+import spidev
+import time
+spi=spidev.SpiDev()
+spi.open(0, 0)
+print(spi.mode)
+spi.max_speed_hz=7629
+message = []
+message.append(65)
+message.append(48)
+message.append(77)
+message.append(49)
+message.append(10)
+print("Start Communication")
+while 1:
+    spi.writebytes(message)
+    time.sleep(5)
+    print("Next")
